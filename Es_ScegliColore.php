434814984eaac90f7b9779630b19148fb42e3b2a
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-03 09:57:39 +0200

    Aggiunto colore ad un paragrafo, con le funzioni

--- Es_ScegliColore.php
+++ Es_ScegliColore.php
@@ -7,6 +7,40 @@
 </head>
 <body>
     <?php
+        setColor();
+
+        function chooseColor($c){
+            switch ($c) {
+                case 'r':
+                    echo "<p style='color: red;'>Questo paragrafo ha la proprietà color con valore red</p>";
+                    break;
+                case 'g':
+                    echo "<p style='color: green;'>Questo paragrafo ha la proprietà color con valore green</p>";
+                    break;
+                case 'b':
+                    echo "<p style='color: blue;'>Questo paragrafo ha la proprietà color con valore blue</p>";
+                    break;
+                
+                default:
+                    echo "<p style='color: black;'>Questo paragrafo ha la proprietà color con valore black</p>";
+                    break;
+            }
+        }
+
+        function setColor(){
+            $color;
+            $num = rand(1,5);
+            if ($num == 1) {
+                $color = "r";
+            } elseif ($num == 2) {
+                $color = "g";
+            } elseif ($num == 3) {
+                $color = "b";
+            } else {
+                $color = "p";
+            }
+            chooseColor($color);
+        }
     ?>
 </body>
 </html>
